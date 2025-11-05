4388d694bdd98e0ea262ab6e8109c2667ab22d61
kukalakunta sasank <kukalakuntasasank@kukalakuntas-MacBook-Pro.local>
2025-11-04 18:59:06 -0600

    Configured shadcn, updated layout and styles, removed unused assets

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,12 @@
+'use client';
+export const ExploreBtn = () => {
+  return (
+    <div>
+        <button type ="button" id="explore-btn"  className="mt-7 mx-auto" onClick={() => console.log('Explore button clicked')}>
+            <a href={"#events"}>
+                Explore Events
+            </a>
+        </button>
+    </div>
+  )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/dist/client/link'
+import Image from 'next/image';
+export const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <Link href='/' className='logo'>
+            <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+
+<p>DevEvent</p>
+            </Link>
+            <ul>
+                <li><Link href ='#'>Home</Link></li>
+                <li><Link href ='#'>Events</Link></li>
+                <li><Link href ='#'>Create Events</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
